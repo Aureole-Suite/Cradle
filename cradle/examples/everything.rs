9997ff9d4c2af7287d4ae257dcbf8e3ecd2609b4
Kyuuhachi <caagr98@gmail.com>
2023-10-15 20:42:47 +0200

    Refactor everything
    Not literally but you get the point

--- cradle/examples/everything.rs
+++ cradle/examples/everything.rs
@@ -1,32 +1,45 @@
-use std::{path::Path, panic::catch_unwind};
+use std::panic::{catch_unwind, RefUnwindSafe};
+use std::path::Path;
 
 use indicatif::ParallelProgressIterator;
 use rayon::prelude::*;
 
 fn main() -> anyhow::Result<()> {
+	run_everything(".itp", |dat| match cradle::itp::read(dat) {
+		Ok(_) => Ok(()),
+		Err(cradle::itp::Error::NotItp) => Ok(()),
+		Err(e) => Err(e.into())
+	})?;
+	Ok(())
+}
+
+fn run_everything(
+	suffix: &str,
+	f: impl Fn(&[u8]) -> anyhow::Result<()> + Send + Sync + RefUnwindSafe,
+) -> anyhow::Result<()> {
 	let stdout = std::process::Command::new("locate")
-		.arg(".itp")
+		.arg(suffix)
 		.output()?
 		.stdout;
 	let stdout = String::from_utf8(stdout)?;
 	let stdout = stdout.lines()
-		.filter(|line| line.ends_with(".itp"))
+		.filter(|line| line.ends_with(suffix))
 		.collect::<Vec<_>>();
 
-	let style = indicatif::ProgressStyle::with_template("{bar} {pos}/{len}")?
+	let style = indicatif::ProgressStyle::with_template("{prefix} {bar} {pos}/{len}")?
 		.progress_chars("█🮆🮅🮄▀🮃🮂▔ ");
 	let bar = indicatif::ProgressBar::new(stdout.len() as _)
-		.with_style(style);
+		.with_style(style)
+		.with_prefix(suffix.to_owned());
 	stdout.par_iter().progress_with(bar.clone()).try_for_each(|line| {
 		let path = Path::new(line);
 		if path.is_file() {
 			let file = std::fs::File::open(path)?;
 			let dat = unsafe { memmap2::Mmap::map(&file)? };
-			let v = catch_unwind(|| cradle::itp::read(&dat));
+			let v = catch_unwind(|| f(&dat));
 			let err = match v {
-				Ok(Ok(_)) => None,
-				Ok(Err(cradle::itp::Error::NotItp)) => None,
-				Ok(Err(e)) => Some(anyhow::Error::from(e)),
+				Ok(Ok(())) => None,
+				Ok(Err(e)) => Some(e),
 				Err(_) => Some(anyhow::anyhow!("panic")),
 			};
 			if let Some(err) = err {
