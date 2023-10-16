b5a1e4904b85021196136f4bdbf73f7210035916
Kyuuhachi <caagr98@gmail.com>
2023-10-17 00:16:34 +0200

    Change Output to contain a single path

--- cradle-cli/src/util.rs
+++ cradle-cli/src/util.rs
@@ -1,33 +1,35 @@
 use camino::{Utf8PathBuf, Utf8Path};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Output<'a> {
-	At { path: &'a Utf8Path },
-	In { dir: &'a Utf8Path, name: &'a str },
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Output {
+	At(Utf8PathBuf),
+	In(Utf8PathBuf),
 }
 
-impl<'a> Output<'a> {
+impl Output {
 	pub fn with_name(&self, name: &str) -> Utf8PathBuf {
 		match self {
-			Output::At { path } => path.to_path_buf(),
-			Output::In { dir, name: _ } => dir.join(name),
+			Output::At(path) => path.to_path_buf(),
+			Output::In(path) => path.with_file_name(name),
 		}
 	}
 
 	pub fn with_extension(&self, ext: &str) -> Utf8PathBuf {
 		match self {
-			Output::At { path } => path.to_path_buf(),
-			Output::In { dir, name } => dir.join(name).with_extension(ext),
+			Output::At(path) => path.to_path_buf(),
+			Output::In(path) => path.with_extension(ext),
 		}
 	}
 
-	pub fn from_output_flag(output: Option<&'a Utf8Path>, file: &'a Utf8Path, n_inputs: usize) -> eyre::Result<Self> {
+	pub fn from_output_flag(output: Option<impl AsRef<Utf8Path>>, file: impl AsRef<Utf8Path>, n_inputs: usize) -> eyre::Result<Self> {
+		let output = output.as_ref().map(|a| a.as_ref());
+		let file = file.as_ref();
 		let dir = if let Some(output) = output {
 			if n_inputs == 1 && !output.as_str().ends_with(std::path::is_separator) {
 				if let Some(parent) = output.parent() {
 					std::fs::create_dir_all(parent)?;
 				}
-				return Ok(Output::At { path: output })
+				return Ok(Output::At(output.to_path_buf()))
 			}
 
 			std::fs::create_dir_all(output)?;
@@ -36,6 +38,6 @@ impl<'a> Output<'a> {
 			file.parent().ok_or_else(|| eyre::eyre!("file has no parent"))?
 		};
 		let name = file.file_name().ok_or_else(|| eyre::eyre!("file has no name"))?;
-		Ok(Output::In { dir, name })
+		Ok(Output::In(dir.join(name)))
 	}
 }
